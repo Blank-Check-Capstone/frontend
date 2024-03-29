1bb55dfbbfc14043b34a30a0b058bcb94de50d9e
wjdgotjd <wjdgotjd529@naver.com>
2024-03-29 11:29:12 +0900

    알림판 버튼 디자인 추가

---
+++ src/components/NoticeBoard/Button.jsx
@@ -0,0 +1,9 @@
+const Button = ({ content }) => {
+  return (
+    <button className="px-[30px] py-[14px] text-2xl bg-[#1D1D1D] border-2 border-[#BFC1C3] text-white">
+      {content}
+    </button>
+  );
+};
+
+export default Button;

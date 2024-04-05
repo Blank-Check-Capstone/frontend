bf3bab358f457dfb1b977805fc5f4a1b135b2386
asteroid-J <speed35363@gmail.com>
2024-04-05 12:23:26 +0900

    사이드바 완성

--- src/pages/OrderBoard.jsx
+++ src/pages/OrderBoard.jsx
@@ -1,17 +1,17 @@
 import Main from "../components/OrderBoard/Main";
-import KoreanFlag from "../assets/images/KoreanFlag.jpg";
+import KoreanFlag from "../assets/images/KoreanFlag.png";
 
 const OrderBoard = () => {
     return (
         <div className="flex w-full h-screen">
             <div className="w-[14%] h-full bg-[#D9D9D9]">
-                <div className="w-[100%] h-[30%] bg-[#000] flex flex-col gap-8 items-center justify-center">
-                    <div className="w-[60%] h-[50%] rounded-[30%] bg-white"></div>
-                    <div className="text-white text-4xl font-semibold">선불1</div>
+                <div className="w-[100%] h-[30%] bg-[#000] flex flex-col gap-3 items-center justify-center">
+                    <div className="w-[60%] h-[60%] rounded-[30%] bg-white"></div>
+                    <div className="text-white text-3xl font-semibold">선불1</div>
                 </div>
                 <div className="h-[70%] flex flex-col justify-between">
 
-                    <div className="h-full">
+                    <div className="h-full flex flex-col">
                         <div className="flex h-[18%] w-[100%] items-center gap-8 border-l-8 border-[#46B0AE] pl-8">
                             <svg xmlns="http://www.w3.org/2000/svg" width="52" height="37" viewBox="0 0 52 37" fill="none">
                                 <rect y="32.3099" width="52" height="4.69014" fill="white"/>
@@ -21,14 +21,22 @@ const OrderBoard = () => {
                             <div>메뉴주문</div>
                         </div>
                         
-                        <div className="flex h-[18%] w-[100%]">
-                            <img src={KoreanFlag} alt="태극기" className="" />
+                        <div className="flex h-[18%] w-[100%] items-center justify-center">
+                            <img src={KoreanFlag} alt="태극기" className="h-[60%] w-[25%] mr-10" />
+                            <div className="mr-5 text-xl">FUN</div>
                         </div>
-                    </div>
-                    <div className="employee_call">
-                        <p className="employee_call_txt">직원<br />호출</p>
-                    </div>
-                    
+                        
+                        <div className="flex h-[18%] w-[100%] items-center justify-center gap-5">
+                            <img src={KoreanFlag} alt="태극기" className="h-[60%] w-[25%] ml-1" />
+                            <div className="mr-5 text-xl pl-3">LANG</div>
+                        </div>
+
+                        <div className="flex flex-1 items-center justify-center mt-[40%]">
+                            <div className="bg-[#46B0AE] text-white rounded-[50%] w-[70%] h-[100%] mb-[35%] flex justify-center items-center">
+                                <div className="text-center">직원<br />호출</div>
+                            </div>
+                        </div>  
+                    </div>    
                 </div>
             </div>
             <Main/>

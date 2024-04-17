80898c0efe03d0256ff4b7d589be092426907223
icerice06 <bin37436899@gmail.com>
2024-04-17 15:10:23 +0900

    직원 호출

--- src/pages/OrderBoard.jsx
+++ src/pages/OrderBoard.jsx
@@ -1,20 +1,21 @@
 import Main from "../components/OrderBoard/Main";
 import Menu from "../components/OrderBoard/Side/Menu";
 import KoreanFlag from "../assets/images/KoreanFlag.png";
-import MenuOrderIcon from "../assets/images/menuOrder.svg"
+import MenuOrderIcon from "../assets/images/menuOrder.svg";
 import Shopping from "../components/OrderBoard/Modal/Shopping";
+import Call from "../components/OrderBoard/Modal/Call";
 
 const menuList = [
-  { id: 1, title: '메뉴주문', icon: MenuOrderIcon, selected: true },
-  { id: 2, title: 'FUN', icon: KoreanFlag, selected: false },
-  { id: 3, title: 'LANG', icon: KoreanFlag, selected: false },
-]
+  { id: 1, title: "메뉴주문", icon: MenuOrderIcon, selected: true },
+  { id: 2, title: "FUN", icon: KoreanFlag, selected: false },
+  { id: 3, title: "LANG", icon: KoreanFlag, selected: false },
+];
 
 const OrderBoard = () => {
   return (
     <div className="flex w-full h-screen">
-
-      <Shopping />
+      {/* <Shopping /> */}
+      <Call />
 
       <div className="fixed flex flex-col top-0 w-v[15vw] h-full bg-[#393939] z-0">
         <div className="w-[100%] h-[20vw] bg-[#000] max-h-60 flex flex-col gap-2 items-center justify-center">
@@ -23,13 +24,17 @@ const OrderBoard = () => {
         </div>
         <div className="flex-1 flex flex-col justify-between">
           <div className="h-full flex flex-col overflow-auto">
-            {
-            /* <Menu title="메뉴주문" icon={<MenuOrderIcon />} selected={true} />
+            {/* <Menu title="메뉴주문" icon={<MenuOrderIcon />} selected={true} />
             <Menu title="FUN" icon={<img src={KoreanFlag} alt="태극기" className="h-[5vw] w-[5vw]" />} selected={false} />
-            <Menu title="LANG" icon={<img src={KoreanFlag} alt="태극기" className="h-[5vw] w-[5vw]" />} selected={false} /> */
-            }
-            {menuList.map(menu => (
-              <Menu key={menu.id} id={menu.id} title={menu.title} icon={menu.icon} selected={menu.selected} />
+            <Menu title="LANG" icon={<img src={KoreanFlag} alt="태극기" className="h-[5vw] w-[5vw]" />} selected={false} /> */}
+            {menuList.map((menu) => (
+              <Menu
+                key={menu.id}
+                id={menu.id}
+                title={menu.title}
+                icon={menu.icon}
+                selected={menu.selected}
+              />
             ))}
           </div>
           <div className="flex flex-1 w-[15vw] h-[15vw] items-center justify-center">

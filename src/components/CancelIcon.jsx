7b44cc8dbde6d26b05f4da462f623f2459c1274c
wjdgotjd <wjdgotjd529@naver.com>
2024-04-25 19:29:35 +0900

    닫기 아이콘 변경

--- src/components/CancelIcon.jsx
+++ src/components/CancelIcon.jsx
@@ -1,5 +1,21 @@
-const CancelIcon = () => {
-    return <svg xmlns="http://www.w3.org/2000/svg" className="stroke-[0.2] text-gray-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
-}
+const CancelIcon = ({ width, height, stokeWidth, color }) => {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      width={width}
+      height={height}
+      fill="none"
+      stroke="currentColor"
+      stroke-width={stokeWidth}
+      color={color}
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="lucide lucide-x"
+    >
+      <path d="M18 6 6 18" />
+      <path d="m6 6 12 12" />
+    </svg>
+  );
+};
 
 export default CancelIcon;
--- src/components/OrderBoard/Modal/Call.jsx
+++ src/components/OrderBoard/Modal/Call.jsx
@@ -1,9 +1,11 @@
-const Call = () => {
+import CancelIcon from "../../CancelIcon";
+
+const Call = ({ closeCallModal }) => {
   return (
     <div className="w-full h-full fixed z-10 flex justify-center items-center">
       <div className="w-[90%] h-[90%]  bg-white relative z-[2]">
         <div className="w-full h-[10%] bg-[#222222] flex items-center justify-between">
-          <div className="text-2xl text-white mx-5 flex">
+          <div className="text-2xl text-white flex">
             <svg
               className="w-[2vw] h-[2.8vw] mx-3"
               width="56"
@@ -23,17 +25,21 @@ const Call = () => {
             </svg>
             직원호출
           </div>
-          <div className="flex text-2xl text-white mx-5">
-            <svg
-              className="w-[2vw] h-[2.8vw] fill-[#F1F1F1] mx-2"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 50 50"
-              width="50px"
-              height="50px"
-            >
-              <path d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z" />
-            </svg>
-            닫기
+          <div
+            className="flex items-center cursor-pointer gap-[1vw]"
+            onClick={closeCallModal}
+          >
+            <div>
+              <CancelIcon
+                width="3vw"
+                height="3vw"
+                stokeWidth="0.2"
+                color="white"
+              />
+            </div>
+            <div className="text-[2vw] text-white leading-[2vw] font-bold">
+              닫기
+            </div>
           </div>
         </div>
 

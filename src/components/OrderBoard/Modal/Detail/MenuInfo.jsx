36be375c2084802caca2f649814f6997e6bbc23a
wjdgotjd <wjdgotjd529@naver.com>
2024-04-25 19:29:21 +0900

    메뉴 상세 모달 컴포넌트 추가

--- src/components/OrderBoard/Modal/Detail/MenuInfo.jsx
+++ src/components/OrderBoard/Modal/Detail/MenuInfo.jsx
@@ -1,8 +1,8 @@
 const MenuInfo = () => {
   return (
-    <div className="w-[30%] h-full flex flex-col gap-[1vw]">
-      <div className="w-full h-[60%] bg-black"></div>
-      <div className="w-full h-[40%] flex flex-col justify-between">
+    <div className="w-[35%] h-full flex flex-col gap-[1vw]">
+      <div className="w-full h-[70%] bg-black"></div>
+      <div className="w-full h-[30%] flex flex-col justify-between">
         <div className="text-[1.5vw]">짜장면</div>
         <div className="text-[1.5vw] font-bold">7,000원</div>
       </div>

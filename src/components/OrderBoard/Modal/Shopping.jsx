fbeed0e7ec60c65bde61e4caddd8df468a4b5b91
wjdgotjd <wjdgotjd529@naver.com>
2024-05-17 09:20:44 +0900

    장바구니 비우기 로직 구현 및 변수 이름 변경

--- src/components/OrderBoard/Modal/Shopping.jsx
+++ src/components/OrderBoard/Modal/Shopping.jsx
@@ -2,11 +2,14 @@ import CancelIcon from "../../CancelIcon";
 import MinusIcon from "../../MinusIcon";
 import PlusIcon from "../../PlusIcon";
 import CheckIcon from "../../CheckIcon";
+import Menu from "./Shopping/Menu";
 
 const Shopping = ({
   closeShoppingModal,
   choiceMenus,
   getMenuByCategoryIdAndMenuId,
+  emptyShopping,
+  addChoiceMenu,
 }) => {
   return (
     <div className="w-full h-full fixed z-10">
@@ -18,7 +21,10 @@ const Shopping = ({
             </div>
           </div>
           <div className="flex items-center gap-[2vw]">
-            <div className="w-[15vw] h-[70%] bg-[#D9D9D9] text-[#929292] rounded-3xl font-light leading-[3vw] text-center px-3 hover: cursor-pointer text-[1.35vw]">
+            <div
+              className="w-[15vw] h-[70%] bg-[#D9D9D9] text-[#929292] rounded-3xl font-light leading-[3vw] text-center px-3 hover: cursor-pointer text-[1.35vw]"
+              onClick={emptyShopping}
+            >
               장바구니 비우기
             </div>
             <div
@@ -30,40 +36,19 @@ const Shopping = ({
           </div>
         </div>
 
-        <div className="w-full h-[55%]">
-          {choiceMenus.map((_menu) => {
-            console.log(_menu);
-
-            const menu = getMenuByCategoryIdAndMenuId(
-              _menu.categoryId,
-              _menu.menuId
+        <div className="w-full h-[55%] overflow-auto">
+          {choiceMenus.map((menu) => {
+            const menuInfo = getMenuByCategoryIdAndMenuId(
+              menu.categoryId,
+              menu.menuId
             );
 
             return (
-              <div className="w-full h-[37%] border-b border-[#E2E2E2] border-solid">
-                <div className="p-[1vw] ml-[0.55vw] flex flex-col gap-[1.7vw]">
-                  <div className="font-bold text-[1.8vw]">{menu.name}</div>
-                  <div className="flex justify-between">
-                    <div className="flex gap-6 items-center">
-                      <div className="border-[#E2E2E2] rounded-3xl border hover: cursor-pointer">
-                        <MinusIcon />
-                      </div>
-                      <p className="text-[1.8vw] font-medium">1</p>
-                      <div className="border-[#E2E2E2] rounded-3xl border hover: cursor-pointer">
-                        <PlusIcon />
-                      </div>
-                    </div>
-                    <div className="flex gap-7 items-end">
-                      <p className="text-[2vw] text-[#6D6D6D] ">
-                        {menu.price.toLocaleString()}원
-                      </p>
-                      <div className="w-[2.7vw] h-[2.7vw] bg-[#D9D9D9] rounded-3xl hover: cursor-pointer mr-[0.3vw] mb-[0.3vw]">
-                        <CancelIcon />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <Menu
+                menu={menu}
+                menuInfo={menuInfo}
+                addChoiceMenu={addChoiceMenu}
+              />
             );
           })}
         </div>
@@ -73,14 +58,29 @@ const Shopping = ({
             <div className="hover: cursor-pointer">
               <CheckIcon />
             </div>
-            <p className="font-bold text-[#5F5F5F] text-[1.25vw]">상품 할인권 사용</p>
+            <p className="font-bold text-[#5F5F5F] text-[1.25vw]">
+              상품 할인권 사용
+            </p>
           </div>
         </div>
 
         <div className="w-full h-[14%] flex justify-between px-[1.8vw]">
-          <div className="text-[#A8A8A8] text-[1.8vw] flex items-center">총 금액</div>
+          <div className="text-[#A8A8A8] text-[1.8vw] flex items-center">
+            총 금액
+          </div>
           <div className="text-3xl mt-[2vw] flex">
-            <p className="font-bold items-center text-[3vw]">7,000</p>
+            <p className="font-bold items-center text-[3vw]">
+              {choiceMenus
+                .reduce((total, choiceMenu) => {
+                  const menu = getMenuByCategoryIdAndMenuId(
+                    choiceMenu.categoryId,
+                    choiceMenu.menuId
+                  );
+
+                  return total + choiceMenu.amount * menu.price;
+                }, 0)
+                .toLocaleString()}
+            </p>
             <p className="text-[2vw] leading-10 items-center">원</p>
           </div>
         </div>
@@ -97,7 +97,10 @@ const Shopping = ({
           </div>
         </div>
       </div>
-      <div className="w-full h-full bg-black opacity-30 absolute top-0 left-0 z-[1]"></div>
+      <div
+        className="w-full h-full bg-black opacity-30 absolute top-0 left-0 z-[1]"
+        onClick={closeShoppingModal}
+      ></div>
     </div>
   );
 };

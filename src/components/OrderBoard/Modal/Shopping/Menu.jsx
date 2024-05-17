55a98d0a11d71df660582355b47f4d76ac75cf90
wjdgotjd <wjdgotjd529@naver.com>
2024-05-17 09:21:09 +0900

    장바구니 메뉴 목록 컴포넌트화

---
+++ src/components/OrderBoard/Modal/Shopping/Menu.jsx
@@ -0,0 +1,37 @@
+import CancelIcon from "../../../CancelIcon";
+import MinusIcon from "../../../MinusIcon";
+import PlusIcon from "../../../PlusIcon";
+
+const Menu = ({ menu, menuInfo, addChoiceMenu }) => {
+  return (
+    <div className="w-full py-[1.2vw] border-b border-[#E2E2E2] border-solid">
+      <div className="p-[1vw] ml-[0.55vw] flex flex-col gap-[1.7vw]">
+        <div className="font-bold text-[1.8vw]">{menuInfo.name}</div>
+        <div className="flex justify-between">
+          <div className="flex gap-6 items-center">
+            <div className="border-[#E2E2E2] rounded-3xl border hover: cursor-pointer">
+              <MinusIcon />
+            </div>
+            <p className="text-[1.8vw] font-medium">{menu.amount}</p>
+            <div
+              className="border-[#E2E2E2] rounded-3xl border hover: cursor-pointer"
+              onClick={() => addChoiceMenu(menu.categoryId, menu.menuId)}
+            >
+              <PlusIcon />
+            </div>
+          </div>
+          <div className="flex gap-7 items-end">
+            <p className="text-[2vw] text-[#6D6D6D] ">
+              {(menuInfo.price * menu.amount).toLocaleString()}원
+            </p>
+            <div className="w-[2.7vw] h-[2.7vw] bg-[#D9D9D9] rounded-3xl hover: cursor-pointer mr-[0.3vw] mb-[0.3vw]">
+              <CancelIcon />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;

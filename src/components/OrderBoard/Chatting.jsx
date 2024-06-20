48ea5a82aa37c3f339823f3b56693f514602b087
wjdgotjd <wjdgotjd529@naver.com>
2024-06-20 17:22:30 +0900

    코드 정리

--- src/components/OrderBoard/Chatting.jsx
+++ src/components/OrderBoard/Chatting.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import * as Hangul from "hangul-js";
 import { t } from "i18next";
 
